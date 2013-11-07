58c3924032e84f7e3fd87b75f4e52bbbe9793dc7
Andy Freeland <andy.freeland@redjack.com>
2013-11-07 00:39:16 -0500

    BUILD THE RPM

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -e
 SCRIPT=$(python -c "import os; print(os.path.realpath('$0'))")
 SCRIPT_DIR=`dirname "$SCRIPT"`
 
+VENV_DIR="$PWD"
+
+# Create a relocatable virtualenv for ansible
 virtualenv ansible
 . ansible/bin/activate
 pip install -r "$SCRIPT_DIR"/requirements.txt
@@ -12,3 +15,30 @@ virtualenv --relocatable ansible
 
 cd ansible/lib/python2.6/site-packages/ansible
 patch -p0 < "$SCRIPT_DIR"/ansible-site-packages.patch
+
+# Setup the directory structure for the RPM
+TMPDIR=`mktemp -d`
+mkdir -p "$TMPDIR"/opt
+
+cp -ap "$VENV_DIR"/ansible "$TMPDIR"/opt/
+cd "$TMPDIR"
+
+find . -name "*.pyc" -delete
+
+mkdir -p usr/local/bin
+cd usr/local/bin
+
+for f in {ansible,ansible-playbook,ansible-doc}; do
+    ln -s ../../../opt/ansible/bin/$f .
+done
+
+# Use FPM to build an RPM
+cd /vagrant
+fpm -s dir -t rpm -n ansible -v 1.3.4-`date +%s` -C "$TMPDIR" \
+    -p ansible-VERSION_ARCH.rpm \
+    -d "postgresql-devel" \
+    -d "python-devel" \
+    usr/local/bin opt
+
+# Cleanup
+rm -rf "$TMPDIR"
